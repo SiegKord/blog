286954c855bfe8813cea75dd972d4f4bf63adb58
Sieg <nicolas.ferrer23@gmail.com>
2017-12-13 23:40:43 +0100

    Création d'un formulaire

---
+++ ajout.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Formulaire</title>
+	</head>
+	
+	<body>
+	
+		<h1>Formulaire de création d'article</h1>
+	
+		<form method="post" action="">
+		
+			<p>Nom de l'article : <input type="text" name="titre" /></p>
+			</br>
+			<p>Nom de l'auteur (peut être laissé vide) : <input type="text" name="auteur" /></p>
+			</br>
+			<p>Tapez votre article : <textarea name="contenu" rows="10" cols="30">
+			</textarea></p>
+			<input type="submit" value="Envoyer" />
+			<input type="reset" value="Effacer" />
+		
+		</form>
+			
+		
+	</body>
+</html>
--- index.php
+++ index.php
@@ -21,16 +21,19 @@
 		$myDao = new ArticleDAO;	
 		$article = $myDao->getArticle(5);
 		echo "Le titre de cet article est : <b>" . $article->getTitle() . "</b>, son auteur est <b>" . $article->getAuthor() . "</b> publié le <b>" . $article->getDatepost() . "</b>. </br> Son contenu est : <b>" . $article->getContent() . "</b>";
-		echo '<br><br><br>';
 		
+		echo '<br><br><br>';
+		echo "------------";
+		echo '<br><br><br>';
 		
 		$idSearch = $myDao->setArticle("Coming-out", "Nann", "2017-11-16 13:25:34", "Je suis gay.");
 		echo "L'article sélectionné est le numéro <b>" . $idSearch->getId() . "</b> créé depuis le lancement du blog.";
+		
+		echo '<br><br><br>';
+		echo "------------";
 		echo '<br><br><br>';
 		
-		
 		$articles = $myDao->get5Articles();
-		var_dump($articles);
 		$i = 1;
 		
 		foreach($articles as $eachArt) {
