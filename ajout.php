0e182d2ea14e9c375e1ae87522414c8b4328953f
Sieg <nicolas.ferrer23@gmail.com>
2017-12-14 16:24:55 +0100

    essai d'implanter fonction Javascript confirm()

--- ajout.php
+++ ajout.php
@@ -9,7 +9,7 @@
 	
 		<h1>Formulaire de création d'article</h1>
 	
-		<form method="post" action="traitement.php" name="formulaire">
+		<form method="post" name="formulaire" action="traitement.php">
 		
 			<p>Nom de l'article : <input type="text" name="titre" /></p>
 			</br>
@@ -17,11 +17,19 @@
 			</br>
 			<p>Tapez votre article : <textarea name="contenu">
 			</textarea></p>
-			<input type="submit" value="Envoyer"/>		
+			<input type="submit" value="Envoyer" onClick="NoAuthor()"/>		
 			<input type="reset" value="Effacer" />
 		
 		</form>
-		
+		<script type="text/javascript">		
+			function NoAuthor(){
+				if (typeOf($_POST['auteur'])=='undefined' or !$_POST['auteur']) {
+					if (confirm("Vous n'avez pas renseigné le champ \"auteur\", voulez-vous publier l'article anonymement ?")){
+						formulaire.submit();
+					}
+				}
+			}
+		</script>	
 		
 	</body>
 </html>
