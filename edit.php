31a19e63d8f731d6443a0802f13c55eb313e14b2
Sieg <nicolas.ferrer23@gmail.com>
2018-01-15 20:30:59 +0100

    Optimisation en cours...

--- edit.php
+++ edit.php
@@ -26,12 +26,12 @@
 			echo "<p>Contenu de l'article : <textarea name=\"contentModified\">" . $article->getContent() . "</textarea></p></br>";
 			echo "<input type=\"submit\" name = \"submit\" value=\"Valider\"/>";
 			echo "</form></br></br>";
-			if(isset($_POST['action'])) {
+			if(isset($_POST['modify'])) {
 				if(!empty($_POST['titleModified']) && !empty($_POST['contentModified'])) {
 					$article->setTitle($_POST['titleModified']);
 					$article->setContent($_POST['contentModified']);
 					$myDAO->setArticle($article);
-				var_dump($updatedArticle);
+				
 				}
 				else {
 					echo "Un ou plusieurs champs sont manquants, veuillez resaisir les informations.";
